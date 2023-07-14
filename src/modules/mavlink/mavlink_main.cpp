35f965b801488793e07da0242f082f49fd744d0b
Konrad <konrad@auterion.com>
2023-07-14 13:49:04 +0200

    kconfig: Add option to enable figure of eight support

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1544,7 +1544,9 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("DEBUG_VECT", 1.0f);
 		configure_stream_local("NAMED_VALUE_FLOAT", 1.0f);
 		configure_stream_local("LINK_NODE_STATUS", 1.0f);
+#ifdef CONFIG_FIGURE_OF_EIGHT
 		configure_stream_local("FIGURE_EIGHT_EXECUTION_STATUS", 2.0f);
+#endif // CONFIG_FIGURE_OF_EIGHT
 #endif // !CONSTRAINED_FLASH
 
 		break;
@@ -1610,7 +1612,9 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("DEBUG_VECT", 10.0f);
 		configure_stream_local("NAMED_VALUE_FLOAT", 10.0f);
 		configure_stream_local("LINK_NODE_STATUS", 1.0f);
+#ifdef CONFIG_FIGURE_OF_EIGHT
 		configure_stream_local("FIGURE_EIGHT_EXECUTION_STATUS", 5.0f);
+#endif // CONFIG_FIGURE_OF_EIGHT
 #endif // !CONSTRAINED_FLASH
 
 		break;
@@ -1672,7 +1676,9 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("DEBUG_VECT", 1.0f);
 		configure_stream_local("NAMED_VALUE_FLOAT", 1.0f);
 		configure_stream_local("LINK_NODE_STATUS", 1.0f);
+#ifdef CONFIG_FIGURE_OF_EIGHT
 		configure_stream_local("FIGURE_EIGHT_EXECUTION_STATUS", 5.0f);
+#endif // CONFIG_FIGURE_OF_EIGHT
 #endif // !CONSTRAINED_FLASH
 
 		break;
@@ -1766,7 +1772,9 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("DEBUG_VECT", 50.0f);
 		configure_stream_local("NAMED_VALUE_FLOAT", 50.0f);
 		configure_stream_local("LINK_NODE_STATUS", 1.0f);
+#ifdef CONFIG_FIGURE_OF_EIGHT
 		configure_stream_local("FIGURE_EIGHT_EXECUTION_STATUS", 5.0f);
+#endif // CONFIG_FIGURE_OF_EIGHT
 #endif // !CONSTRAINED_FLASH
 
 		break;
@@ -1844,7 +1852,9 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("DEBUG_FLOAT_ARRAY", 1.0f);
 		configure_stream_local("DEBUG_VECT", 1.0f);
 		configure_stream_local("NAMED_VALUE_FLOAT", 1.0f);
+#ifdef CONFIG_FIGURE_OF_EIGHT
 		configure_stream_local("FIGURE_EIGHT_EXECUTION_STATUS", 5.0f);
+#endif // CONFIG_FIGURE_OF_EIGHT
 #endif // !CONSTRAINED_FLASH
 		break;
 
